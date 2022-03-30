926a534efc21f5dd425e7693bceeb2ce397cd4b3
michael <yhm2046@gmail.com>
2022-03-31 00:05:41 +0800

    添加下拉菜单打折功能

--- app/src/main/java/com/example/strategypattern/MainActivity.kt
+++ app/src/main/java/com/example/strategypattern/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.strategypattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Switch
 import com.example.strategypattern.databinding.ActivityMainBinding
 
 /**
@@ -9,6 +14,9 @@ import com.example.strategypattern.databinding.ActivityMainBinding
  * 策略模式Strategy Pattern
  */
 class MainActivity : AppCompatActivity() {
+    var TAG = "MainActivity_tag"
+    var discount_num = 0.0
+    var discount_str = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
@@ -18,7 +26,19 @@ class MainActivity : AppCompatActivity() {
             var siglePrice = binding.etSinglePrice.text.toString()
             var amount = binding.etAmount.text.toString()
             var sumPrice = siglePrice.toDouble() *  amount.toInt()
-            val str = "sigle price: " + siglePrice.toDouble() + " amount: " + amount.toInt() + "\nsum: " +  sumPrice
+
+            when(discount_num){
+                    0.0 -> discount_str = "正常"
+                    0.8 -> {
+                        sumPrice *= 0.8
+                        discount_str = "打八折"
+                    }
+                    0.5 -> {
+                        sumPrice *= 0.5
+                        discount_str = "打五折"
+                    }
+                }
+            val str = "sigle price: " + siglePrice.toDouble() + " amount: " + amount.toInt() + "\n"+discount_str + " sum: " +  sumPrice
             binding.etDetail.setText(str)
             binding.tvSumPrice.setText(sumPrice.toString())
         }
@@ -27,6 +47,26 @@ class MainActivity : AppCompatActivity() {
             binding.etAmount.setText("")
             binding.etDetail.setText("")
             binding.tvSumPrice.setText("")
+        }
+//        discount
+        val discount = resources.getStringArray(R.array.Discount)
+        if (binding.spDiscount != null) {
+            val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,discount)
+            binding.spDiscount.adapter = adapter
+            binding.spDiscount.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    when(discount[position]){
+                        "NO" -> discount_num
+                        "80%" -> discount_num = 0.8
+                        "50%" -> discount_num = 0.5
+                    }
+                    Log.i(TAG,"click " + discount_num)
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
         }
     }
 }
